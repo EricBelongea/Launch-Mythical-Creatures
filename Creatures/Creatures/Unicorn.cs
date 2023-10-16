265eaf5b03cf33f7ccd5a538e83a78d99bf8eef2
Richard T <richard@turing.edu>
2023-10-16 17:20:18 -0400

    New Unicorn class

--- Creatures/Creatures/Unicorn.cs
+++ Creatures/Creatures/Unicorn.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mythical_Creatures
+namespace MythicalCreatures.Creatures
 {
-    public class Unicorn
+    internal class Unicorn
     {
 
     }
