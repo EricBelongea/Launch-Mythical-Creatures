14baca05325054a1bc87646fcfedca722509c1da
Eric Belongea <eric.belongea@gmail.com>
2024-03-06 09:55:13 -0700

    Vampire class & tests completed.

--- Creatures/Creatures/Vampie.cs
+++ Creatures/Creatures/Vampie.cs
@@ -1,10 +1,21 @@
-﻿using System;
-namespace MythicalCreatures.Creatures
+﻿namespace MythicalCreatures.Creatures
 {
-	public class Vampie
+	public class Vampire
 	{
-		public Vampie()
+		public string Name { get; private set; }
+		public string Pet { get; private set; }
+		public bool IsThirsty { get; private set; }
+
+		public Vampire(string name, string pet = "bat")
 		{
+			Name = name;
+			Pet = pet;
+			IsThirsty = true;
+		}
+
+		public void Drink()
+		{
+			IsThirsty = false;
 		}
 	}
 }
